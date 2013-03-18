225ba3117478e6278e1e63ca1c790a4d23cfde6c
Armando Blancas <abm221617@gmail.com>
2013-03-17 22:10:22 -0700

    Bumped up versions.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
 	    :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/blancas/eisen"
   :plugins [[codox "0.6.4"]]
-  :dependencies [[org.clojure/clojure "1.5.0"]
-		 [org.blancas/kern "0.5.0"]
-		 [org.blancas/morph "0.1.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+		 [org.blancas/kern "0.6.0"]
+		 [org.blancas/morph "0.2.0"]]
   :source-paths ["src/main/clojure" "src/main/resources"]
   :codox {:sources ["src/main/clojure"] :output-dir "codox"}
   :jvm-opts ["-Dfile.encoding=UTF-8"])

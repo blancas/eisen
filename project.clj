9606f6ba17dfeabbf2f6e88cd314fcc7f68643a4
Armando Blancas <abm221617@gmail.com>
2013-04-09 22:05:41 -0700

    Bumped up vesions.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.blancas/eisen "0.1.0"
+(defproject org.blancas/eisen "0.2.0"
   :description "A Language for Programmable Applications"
   :license {:name "Eclipse Public License"
 	    :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/blancas/eisen"
   :dependencies [[org.clojure/clojure "1.5.1"]
 		 [org.clojure/core.match "0.2.0-alpha12"]
-		 [org.blancas/kern "0.6.1"]
+		 [org.blancas/kern "0.7.0"]
 		 [org.blancas/morph "0.2.0"]]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]

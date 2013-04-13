5482c623f4fc306cd2b4855d1489b4f205aaa17d
Armando Blancas <abm221617@gmail.com>
2013-04-13 11:05:43 -0700

    Tweaked the val docstring.

--- src/main/clojure/blancas/eisen/parser.clj
+++ src/main/clojure/blancas/eisen/parser.clj
@@ -735,7 +735,7 @@ Literal values follow the rules of Java and Clojure."
   "Parses a declaration for a named value.
 
    'val' identifier '=' expression ( ';' )+
-   ( identifier '=' expression ( ';' )+ )*"
+       ( identifier '=' expression ( ';' )+ )*"
   (>> (word "val")
       (sep-end-by1 semi
         (bind [name identifier  _ (sym \=) val expr]
